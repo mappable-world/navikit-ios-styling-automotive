e08cffa509252598cb76058dd52dcff161a3a1cd
MappableWorld <tech-mobile@mappable.world>
2025-02-18 15:34:59 +0300

    Publish swift package MMKStylingAutomotiveNavigation version 4.11.0

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "MMKStylingAutomotiveNavigation",
+    defaultLocalization: "en",
+    platforms: [.iOS("12.0")],
+    products: [
+        .library(
+            name: "MMKStylingAutomotiveNavigation",
+            type: .static,
+            targets: [
+                "MMKStylingAutomotiveNavigation",
+                "MMKStylingAutomotiveNavigationResources"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/mappable-world/navikit-ios", exact: "4.11.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MMKStylingAutomotiveNavigation",
+            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingAutomotiveNavigation-4.11.0.framework.zip",
+            checksum: "398fce49c8245607473c775c5edeac41daac1b5c19fb3db3b57598032840814b"
+        ),
+        .target(
+            name: "MMKStylingAutomotiveNavigationResources",
+            dependencies: [
+                .product(name: "MappableMobileNavikit", package: "navikit-ios"),
+            ],
+            path: "Resources",
+            resources: [.process("Contents")]
+        )
+    ]
+)
---
+++ Resources/Resources.m
@@ -0,0 +1,13 @@
+#import <resource_bundle_accessor.h>
+
+@interface MMKStylingAutomotiveNavigationSwiftResources : NSObject
+@end
+
+@implementation MMKStylingAutomotiveNavigationSwiftResources
+
++ (NSBundle *)mapkitResourcesBundle
+{
+    return SWIFTPM_MODULE_BUNDLE;
+}
+
+@end
