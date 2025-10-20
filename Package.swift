2adbc2636220571de366952d32824006694064cc
MappableWorld <tech-mobile@mappable.world>
2025-10-20 16:41:11 +0300

    Publish swift package MMKStylingAutomotiveNavigation version 4.25.0

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/mappable-world/navikit-ios", exact: "4.17.2"),
+        .package(url: "https://github.com/mappable-world/navikit-ios", exact: "4.25.0"),
     ],
     targets: [
         .binaryTarget(
             name: "MMKStylingAutomotiveNavigation",
-            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingAutomotiveNavigation-4.17.2.framework.zip",
-            checksum: "107851a4792e2c5fd8a6e4818486f04389b8a09646a1ba82164e6c65a3ae8d23"
+            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingAutomotiveNavigation-4.25.0.framework.zip",
+            checksum: "bdad3521b9cdaf81a30c71f4ebc644826bf26dc109bd45e4abcf428a080f549f"
         ),
         .target(
             name: "MMKStylingAutomotiveNavigationResources",
